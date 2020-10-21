e508a39c596e20be212bd04b5e09e7f4fc2394a9
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2020-10-21 20:11:07 +0900

    feat: API 호출

---
+++ api/api.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// API 호출
+func Request(apiUrl string, params url.Values) ([]byte, error) {
+	baseUrl, err := url.Parse(apiUrl) // string에서 URL로 변환
+
+	if err != nil {
+		return nil, err
+	}
+
+	params.Add("type", "json")         // json 타입으로 호출
+	baseUrl.RawQuery = params.Encode() // 파라미터 삽입
+
+	req, err := http.Get(baseUrl.String()) // GET으로 API 요청
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body) // 바이트 배열로 저장
+
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
