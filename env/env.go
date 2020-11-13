cd38fdfab2ccb1b92db4429eabacce1fd6a34fed
zeroday0619 <zeroday0619@kakao.com>
2020-11-13 11:14:51 +0900

    feat: MySQL 연동 일부 구현

--- env/env.go
+++ env/env.go
@@ -6,13 +6,27 @@ import (
 	"strings"
 )
 
-var BotToken, SchoolInfoKey, SchoolScheduleKey, ElsTimetableKey, MisTimetableKey, HisTimetableKey, MealServiceDietInfoKey string
+var (
+	SchoolInfoKey          string
+	SchoolScheduleKey      string
+	ElsTimetableKey        string
+	MisTimetableKey        string
+	HisTimetableKey        string
+	MealServiceDietInfoKey string
+	BotToken               string
+	DBUser				   string // MySQL Username
+	DBPwd				   string // MySQL Password
+	DBUrl				   string // MySQL Server URL
+	DBEngine 			   string // MySQL DB Engine
+	DBName 				   string // MySQL DATABASE Name
+)
 
 // 환경 변수 처리
 func init() {
 	exists := make(map[string]bool)
 
 	// 환경 변수 값, 환경 변수 존재 여부
+	// API Credentials
 	BotToken, exists["BOT_TOKEN"] = os.LookupEnv("BOT_TOKEN")
 	SchoolInfoKey, exists["SCHOOL_INFO_KEY"] = os.LookupEnv("SCHOOL_INFO_KEY")
 	SchoolScheduleKey, exists["SCHOOL_SCHEDULE_KEY"] = os.LookupEnv("SCHOOL_SCHEDULE_KEY")
@@ -20,6 +34,12 @@ func init() {
 	MisTimetableKey, exists["MIS_TIME_TABLE_KEY"] = os.LookupEnv("MIS_TIME_TABLE_KEY")
 	HisTimetableKey, exists["HIS_TIME_TABLE_KEY"] = os.LookupEnv("HIS_TIME_TABLE_KEY")
 	MealServiceDietInfoKey, exists["MEAL_SERVICE_DIET_INFO_KEY"] = os.LookupEnv("MEAL_SERVICE_DIET_INFO_KEY")
+	// MySQL Credentials
+	DBUser, exists["MySQL_CREDENTIAL_USERNAME"] = os.LookupEnv("MySQL_CREDENTIAL_USERNAME")
+	DBPwd, exists["MySQL_CREDENTIAL_PASSWORD"] = os.LookupEnv("MySQL_CREDENTIAL_PASSWORD")
+	DBUrl, exists["MySQL_SERVER_URL"] = os.LookupEnv("MySQL_SERVER_URL")
+	DBEngine, exists["MySQL_DB_ENGINE"] = os.LookupEnv("MySQL_DB_ENGINE")
+	DBName, exists["MySQL_DB_NAME"] = os.LookupEnv("MySQL_DB_NAME")
 
 	// 존재하지 않는 환경 변수 기록
 	var missingEnv []string
