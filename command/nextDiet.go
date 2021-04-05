0f3d64a9a8873abba71f9aa172f6a5c41020064d
Euiseo Cha <zeroday0619@kakao.com>
2021-04-05 10:44:22 +0900

    fix: index out range 예외처리

--- command/nextDiet.go
+++ command/nextDiet.go
@@ -9,27 +9,35 @@ import (
 
 
 func NextDiet(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-	schoolInfo, err := api.GetSchoolInfoByName(args[1])
-
-	if err != nil {
-		_, err := s.ChannelMessageSend(m.ChannelID, "학교를 찾을 수 없습니다.")
+	if len(args) < 2 {
+		log.Warning("학교가 지정되지 않았습니다.")
+		_, err := s.ChannelMessageSend(m.ChannelID, "학교가 지정되지 않았습니다.")
 		if err != nil {
 			log.Warning(err)
 		}
-	}
+	} else {
+		schoolInfo, err := api.GetSchoolInfoByName(args[1])
 
-	nextDietEmbed, err := embed.GetNextDietEmbed(schoolInfo, time.Now())
+		if err != nil {
+			_, err := s.ChannelMessageSend(m.ChannelID, "학교를 찾을 수 없습니다.")
+			if err != nil {
+				log.Warning(err)
+			}
+		}
 
-	if err != nil {
-		_, err := s.ChannelMessageSend(m.ChannelID, "금일 급식이 없습니다.")
+		nextDietEmbed, err := embed.GetNextDietEmbed(schoolInfo, time.Now())
+
+		if err != nil {
+			_, err := s.ChannelMessageSend(m.ChannelID, "금일 급식이 없습니다.")
+			if err != nil {
+				log.Warning(err)
+			}
+			return
+		}
+
+		_, err = s.ChannelMessageSendEmbed(m.ChannelID, nextDietEmbed)
 		if err != nil {
 			log.Warning(err)
 		}
-		return
-	}
-
-	_, err = s.ChannelMessageSendEmbed(m.ChannelID, nextDietEmbed)
-	if err != nil {
-		log.Warning(err)
 	}
 }
--- command/weeklyDiet.go
+++ command/weeklyDiet.go
@@ -8,26 +8,34 @@ import (
 )
 
 func WeeklyDiet(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-	schoolInfo, err := api.GetSchoolInfoByName(args[1])
-
-	if err != nil {
-		_, err = s.ChannelMessageSend(m.ChannelID, "학교를 찾을 수 없습니다.")
+	if len(args) < 2 {
+		log.Warning("학교가 지정되지 않았습니다.")
+		_, err := s.ChannelMessageSend(m.ChannelID, "학교가 지정되지 않았습니다.")
 		if err != nil {
 			log.Warning(err)
 		}
-	}
+	} else {
+		schoolInfo, err := api.GetSchoolInfoByName(args[1])
 
-	weeklyDietEmbed, err := embed.GetWeeklyDietEmbed(schoolInfo, time.Now())
+		if err != nil {
+			_, err = s.ChannelMessageSend(m.ChannelID, "학교를 찾을 수 없습니다.")
+			if err != nil {
+				log.Warning(err)
+			}
+		}
 
-	if err != nil {
-		_, err = s.ChannelMessageSend(m.ChannelID, "이번 주는 급식이 없습니다.")
+		weeklyDietEmbed, err := embed.GetWeeklyDietEmbed(schoolInfo, time.Now())
+
+		if err != nil {
+			_, err = s.ChannelMessageSend(m.ChannelID, "이번 주는 급식이 없습니다.")
+			if err != nil {
+				log.Warning(err)
+			}
+		}
+
+		_, err = s.ChannelMessageSendEmbed(m.ChannelID, weeklyDietEmbed)
 		if err != nil {
 			log.Warning(err)
 		}
-	}
-
-	_, err = s.ChannelMessageSendEmbed(m.ChannelID, weeklyDietEmbed)
-	if err != nil {
-		log.Warning(err)
 	}
 }
