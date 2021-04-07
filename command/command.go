3b4dbaf8b064ce96e1c0983890241b5a363ba79a
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-04-07 21:10:08 +0900

    feat: 급식 명령어 하나로 통일

--- command/command.go
+++ command/command.go
@@ -3,13 +3,12 @@ package command
 import (
 	"SchoolDay/db"
 	"SchoolDay/extension"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 var log = extension.Log()
-
 var conn, err = db.Database()
-
 
 type Command struct {
 	Exec        func(*discordgo.Session, *discordgo.MessageCreate, []string) // 명령어 함수
@@ -24,23 +23,13 @@ var Commands = map[string]Command{
 		Usage:       []string{""},
 	},
 	"학교등록": {
-		Exec: AddSchool,
-		Description: "학교 등록",
-		Usage: []string{"학교명", "학년", "반"},
+		Exec:        AddSchool,
+		Description: "학교와 학급 등록",
+		Usage:       []string{"학교명", "학년", "반"},
 	},
 	"급식": {
-		Exec:        NextDiet,
-		Description: "다음 급식 식단 출력",
-		Usage:       []string{"학교명"},
-	},
-	"일간급식": {
 		Exec:        DailyDiet,
-		Description: "오늘 급식 식단 출력",
-		Usage:       []string{"학교명"},
-	},
-	"주간급식": {
-		Exec:        WeeklyDiet,
-		Description: "이번 주 급식 식단 출력",
-		Usage:       []string{"학교명"},
+		Description: "급식 식단 출력",
+		Usage:       []string{"학교명", "날짜", "식사종류"},
 	},
 }
