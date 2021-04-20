57788ca2c8a9b57413ca586e91bb6a0bcfb577bd
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-04-20 15:32:14 +0900

    feat: db 함수 매개변수 변경사항 반영

--- command/addSchool.go
+++ command/addSchool.go
@@ -4,26 +4,19 @@ import (
 	"SchoolDay/api"
 	"SchoolDay/db"
 	"SchoolDay/extension"
+	"strconv"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/volatiletech/null/v8"
-	"strconv"
 )
-
-// discordId 			string
-// scCode 				string
-// scGrade 				string
-// scClass 				string
-// scheduleChannelId 	string
-// timetableChannelId 	string
-// dietChannelId		string
 
 func AddSchool(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
 	if len(args) < 4 {
-		extension.ChannelMessageSend(s, m, "사용법: %s 학교등록 학교명 학년 반", "%")
+		extension.ChannelMessageSend(s, m, "사용법: `%s학교등록 학교명 학년 반`", "%")
 		return
 	}
 
-	var discordId = m.Author.ID
+	discordId := m.Author.ID
 	schoolInfo, err := api.GetSchoolInfoByName(args[1])
 
 	if err != nil {
@@ -61,38 +54,34 @@ func AddSchool(s *discordgo.Session, m *discordgo.MessageCreate, args []string) 
 		return
 	}
 
-	var AcGrade int8
-	AcGrade = int8(grade)
-	scGrade := null.Int8From(AcGrade)
+	scCode := schoolInfo["SD_SCHUL_CODE"]
+	scGrade := null.Int8{Int8: int8(grade), Valid: true}
+	scClass := null.Int8{Int8: int8(class), Valid: true}
 
-	var BcClass int8
-	BcClass = int8(class)
-	scClass := null.Int8From(BcClass)
-
-	scheduleTime  := null.Time{}
-	timetableTime := null.Time{}
-	breakfastTime := null.Time{}
-	lunchTime     := null.Time{}
-	dinnerTime    := null.Time{}
-
-	_, err = db.UserAdd(
-		discordId,
-		schoolInfo["SD_SCHUL_CODE"],
-		scGrade,
-		scClass,
-		scheduleTime,
-		timetableTime,
-		breakfastTime,
-		lunchTime,
-		dinnerTime,
-		)
+	user, err := db.UserGet(discordId)
+	var upsertErr error
 
 	if err != nil {
+		_, upsertErr = db.UserAdd(
+			discordId,
+			scCode,
+			scGrade,
+			scClass,
+		)
+	} else {
+		user.ScCode = scCode
+		user.ScGrade = scGrade
+		user.ScClass = scClass
+
+		_, upsertErr = db.UserUpdate(discordId, user)
+	}
+
+	if upsertErr != nil {
 		log.Fatalln(err)
 
 		extension.ChannelMessageSend(s, m, "학교 등록에 실패했습니다.")
 		return
 	}
 
-	extension.ChannelMessageSend(s, m, "등록 완료: `%s %d학년 %d반`", schoolInfo["SCHUL_NM"], scGrade.Int8, scClass.Int8)
+	extension.ChannelMessageSend(s, m, "등록 완료: `%s %d학년%d반`", schoolInfo["SCHUL_NM"], scGrade.Int8, scClass.Int8)
 }
