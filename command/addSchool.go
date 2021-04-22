496fe35d7fb96e22ff6786deb5089950330b4d2e
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-04-22 17:43:01 +0900

    feat: extension의 ChannelMessageSend() 매개변수 변경한 사항 적용

--- command/addSchool.go
+++ command/addSchool.go
@@ -11,8 +11,10 @@ import (
 )
 
 func AddSchool(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	channelId := m.ChannelID
+
 	if len(args) < 4 {
-		extension.ChannelMessageSend(s, m, "사용법: `%s학교등록 학교명 학년 반`", "%")
+		extension.ChannelMessageSend(s, channelId, "사용법: `%s학교등록 학교명 학년 반`", "%")
 		return
 	}
 
@@ -20,7 +22,7 @@ func AddSchool(s *discordgo.Session, m *discordgo.MessageCreate, args []string) 
 	schoolInfo, err := api.GetSchoolInfoByName(args[1])
 
 	if err != nil {
-		extension.ChannelMessageSend(s, m, "학교를 찾을 수 없습니다: `%s`", args[1])
+		extension.ChannelMessageSend(s, channelId, "학교를 찾을 수 없습니다: `%s`", args[1])
 		return
 	}
 
@@ -30,13 +32,13 @@ func AddSchool(s *discordgo.Session, m *discordgo.MessageCreate, args []string) 
 		argNum, _ := strconv.Atoi(args[2])
 
 		if argNum > 6 {
-			extension.ChannelMessageSend(s, m, "학년이 너무 높습니다: `%s`", args[2])
+			extension.ChannelMessageSend(s, channelId, "학년이 너무 높습니다: `%s`", args[2])
 			return
 		}
 
 		grade = argNum
 	} else {
-		extension.ChannelMessageSend(s, m, "학년 입력이 잘못 됐습니다: `%s`", args[2])
+		extension.ChannelMessageSend(s, channelId, "학년 입력이 잘못 됐습니다: `%s`", args[2])
 		return
 	}
 
@@ -44,13 +46,13 @@ func AddSchool(s *discordgo.Session, m *discordgo.MessageCreate, args []string) 
 		argNum, _ := strconv.Atoi(args[3])
 
 		if argNum > 16 {
-			extension.ChannelMessageSend(s, m, "반이 너무 큽니다: `%s`", args[3])
+			extension.ChannelMessageSend(s, channelId, "반이 너무 큽니다: `%s`", args[3])
 			return
 		}
 
 		class = argNum
 	} else {
-		extension.ChannelMessageSend(s, m, "반 입력이 잘못 됐습니다: `%s`", args[3])
+		extension.ChannelMessageSend(s, channelId, "반 입력이 잘못 됐습니다: `%s`", args[3])
 		return
 	}
 
@@ -79,9 +81,9 @@ func AddSchool(s *discordgo.Session, m *discordgo.MessageCreate, args []string) 
 	if upsertErr != nil {
 		log.Fatalln(err)
 
-		extension.ChannelMessageSend(s, m, "학교 등록에 실패했습니다.")
+		extension.ChannelMessageSend(s, channelId, "학교 등록에 실패했습니다.")
 		return
 	}
 
-	extension.ChannelMessageSend(s, m, "등록 완료: `%s %d학년%d반`", schoolInfo["SCHUL_NM"], scGrade.Int8, scClass.Int8)
+	extension.ChannelMessageSend(s, channelId, "등록 완료: `%s %d학년%d반`", schoolInfo["SCHUL_NM"], scGrade.Int8, scClass.Int8)
 }
