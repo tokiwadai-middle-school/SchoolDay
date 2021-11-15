8900551e485969f573466598c13a9944f222b779
root <zetafie125@gmail.com>
2021-11-15 11:02:42 +0900

    feat: SSL 인증 오류 무시

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"crypto/tls"
 )
 
 var log = extension.Log()
@@ -17,22 +18,28 @@ func Request(apiUrl string, params url.Values) ([]byte, error) {
 		return nil, err
 	}
 
+	// 보안 무시
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
 	params.Add("type", "json")         // json 타입으로 호출
 	baseUrl.RawQuery = params.Encode() // 파라미터 삽입
 
-	req, err := http.Get(baseUrl.String()) // GET으로 API 요청
+	client := &http.Client{Transport: tr}
+	res, err := client.Get(baseUrl.String()) // HTTP GET 요청
 
 	if err != nil {
 		return nil, err
 	}
 
 	defer func() {
-		err = req.Body.Close()
+		err = res.Body.Close()
 		if err != nil {
 			log.Error(err)
 		}
 	}()
-	body, err := ioutil.ReadAll(req.Body) // 바이트 배열로 저장
+	body, err := ioutil.ReadAll(res.Body) // 바이트 배열로 저장
 
 	if err != nil {
 		return nil, err
